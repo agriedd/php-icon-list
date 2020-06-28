52824871891af9718091346397a0b731e9450343
Agri Edd. Oematan <dnr739528@gmail.com>
2020-06-28 09:26:22 +0800

    add alert copy status

--- index.php
+++ index.php
@@ -45,6 +45,25 @@
                         <div class="alert">
                             <input type="text" class="form-control" placeholder="Temukan..." v-model="search">
                         </div>
+                        <div v-if="status" class="alert">
+                            <div class="card bg-dark shadow-sm border-0">
+                                <div class="card-body">
+                                    <div class="d-flex">
+                                        <div class="pr-3 text-light">
+                                            <svg class="bi bi-clipboard" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                            <path fill-rule="evenodd" d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
+                                            <path fill-rule="evenodd" d="M9.5 1h-3a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
+                                            </svg>
+                                        </div>
+                                        <div>
+                                            <div class="small text-light h-100 justify-content-center d-flex flex-column">
+                                                Berhasil disalin!
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="mb-5 col-12">
@@ -79,6 +98,7 @@
             data: {
                 data: [],
                 search: "",
+                status: false,
             },
             computed: {
                 icons: function(){
@@ -102,6 +122,10 @@
                     text.setSelectionRange(0, 99999);
                     document.execCommand('copy');
                     document.body.removeChild(text);
+                    this.status = true;
+                    setTimeout(()=>{
+                        this.status = false;
+                    }, 5000);
                 }
             },
             created(){
